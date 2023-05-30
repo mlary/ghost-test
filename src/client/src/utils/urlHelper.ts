88dc98f79ad319b0f5733974a9fdab7e79fd8945
Michael Larin <mklarin@mail.ru>
2023-05-30 21:49:01 +0500

    fix

--- src/client/src/utils/urlHelper.ts
+++ src/client/src/utils/urlHelper.ts
@@ -1,7 +1,7 @@
 export const getLinkedInProfileFromUrl = (url: string) => {
   const urlObj = new URL(url);
   const segments = urlObj.pathname.split('/');
-  const profileId = segments[segments.length - 2];
+  const profileId = segments[2];
   const normalizedProfileId = profileId.split("?");
   return normalizedProfileId[0];
 };
